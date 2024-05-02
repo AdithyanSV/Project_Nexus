5931bb0a67161a4c55dda9526c8bf61388be7cde
Adithyan SV <adithyansv1304@gmail.com>
2024-05-02 13:17:29 +0530

    Unified all login pages and their css files. Also added soe functionality in js

--- Frontend/Login/login.js
+++ Frontend/Login/login.js
@@ -1,7 +1,9 @@
-let signup = document.getElementById("signup");
-let login = document.getElementById("login");
-let forgot = document.getElementById("forgot");
-let goBack = document.getElementById("goBack");
+const signup = document.getElementById("signup");
+const login = document.getElementById("login");
+const forgot = document.getElementById("forgot");
+const goBack = document.getElementById("goBack");
+const goBackBtn = document.getElementById("goBackBtn");
+
 
 signup.addEventListener("click", () => {
     document.querySelector(".form.register").style.display = "flex";
@@ -30,3 +32,26 @@ goBack.addEventListener("click", () => {
     document.querySelector(".form.register").style.display = "none";
     document.querySelector(".form.goBack").style.display = "none";
 });
+
+goBackBtn.addEventListener("click", () => {
+    document.querySelector(".form.forgot").style.display = "none";
+    document.querySelector(".form.signin").style.display = "flex";
+    document.querySelector(".form.register").style.display = "none";
+    document.querySelector(".form.goBack").style.display = "none";
+});
+
+
+function isEmailValid(email) {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  }
+
+    function validateEmail() {
+        const email = document.getElementById("verifyMail").value;
+        if (isEmailValid(email)) {
+            document.querySelector(".form.forgot").style.display = "none";
+            document.querySelector(".form.signin").style.display = "none";
+            document.querySelector(".form.register").style.display = "none";
+            document.querySelector(".form.goBack").style.display = "flex";
+            console.log("Email is valid");
+        }}
